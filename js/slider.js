bda5a36f2336df2faa2c84c202453189e35bab5e
Ahmed Hatem <ahmedhatemezzathelal@gmail.com>
2023-02-01 05:02:27 +0200

    finished

--- js/slider.js
+++ js/slider.js
@@ -1,55 +1,51 @@
-let currentDiscoverSlider = 0,
-    currentDecorSlider = 0,
-    discoverSliderLength = $(".discover .slider img").length,
-    decorSliderLength = $(".decor-discover .slider img").length,
-    discoverNext = $(".discover-next"),
-    decorNext = $(".decor-next");
+var lengths = {
+        discover: $("#discover .slider img").length,
+        decor: $(".decor-discover .slider img").length,
+        metal: $("#discover-metal .slider img").length,
+    },
+    currents = {
+        discover: 0,
+        decor: 0,
+        metal: 0,
+    };
 
-/* slider part start */
-
-function slideTo(order, slider, ar = false) {
-    if (slider == "discover") {
-        $("#discover-slider").css({
-            transform: `translateX(${ar ? `-` : ``}${
-                (100 / discoverSliderLength) * order
-            }%)`,
-        });
-        currentDiscoverSlider = order;
-    } else if (slider == "decor") {
-        $("#decor-slider").css({
-            transform: `translateX(${ar ? `-` : ``}${
-                (100 / decorSliderLength) * order
-            }%)`,
-        });
-        currentDecorSlider = order;
+function slideTo(slider, ar = false) {
+    switch (slider) {
+        case "discover":
+            $("#discover-slider").css({
+                transform: `translateX(${ar ? `-` : ``}${
+                    (100 / lengths[slider]) * currents[slider]
+                }%)`,
+            });
+            break;
+        case "decor":
+            $("#decor-slider").css({
+                transform: `translateX(${ar ? `-` : ``}${
+                    (100 / lengths[slider]) * currents[slider]
+                }%)`,
+            });
+            break;
+        case "metal":
+            $("#discover-slider-metal").css({
+                transform: `translateX(${ar ? `` : `-`}${
+                    (100 / lengths[slider]) * currents[slider]
+                }%)`,
+            });
+            break;
     }
 }
 
-function slideDiscoverStep(condition, ar = false) {
+function slideStep(slider, condition, ar = false) {
     if (condition == "increase") {
-        currentDiscoverSlider == discoverSliderLength - 1
-            ? (currentDiscoverSlider = 0)
-            : (currentDiscoverSlider += 1);
-        slideTo(currentDiscoverSlider, "discover", ar);
+        currents[slider] == lengths[slider] - 1
+            ? (currents[slider] = 0)
+            : (currents[slider] += 1);
+        slideTo(slider, ar);
     } else {
-        currentDiscoverSlider == 0
-            ? (currentDiscoverSlider = discoverSliderLength - 1)
-            : (currentDiscoverSlider -= 1);
-        slideTo(currentDiscoverSlider, "discover", ar);
-    }
-}
-
-function slideDecorStep(condition, ar = false) {
-    if (condition == "increase") {
-        currentDecorSlider == decorSliderLength - 1
-            ? (currentDecorSlider = 0)
-            : (currentDecorSlider += 1);
-        slideTo(currentDecorSlider, "decor", ar);
-    } else {
-        currentDecorSlider == 0
-            ? (currentDecorSlider = decorSliderLength - 1)
-            : (currentDecorSlider -= 1);
-        slideTo(currentDecorSlider, "decor", ar);
+        currents[slider] == 0
+            ? (currents[slider] = lengths[slider] - 1)
+            : (currents[slider] -= 1);
+        slideTo(slider, ar);
     }
 }
 
